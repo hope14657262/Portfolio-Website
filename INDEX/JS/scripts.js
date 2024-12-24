8fbbb146326ccae2af4d8522e604e99ea78ccb75
hope14657262 <hopemelese89@gmail.com>
2024-12-24 21:12:08 +0300

    create my personal web site

---
+++ INDEX/JS/scripts.js
@@ -0,0 +1,8 @@
+// console.log("hello world")
+var typed = new Typed(".typing",{
+    strings:[" " ,"Web Developer","Web Designer","Full- Stack Developer","YouTuber"],
+    typespeed:100,
+    BackSpeed:100,
+    loop:true
+
+})
